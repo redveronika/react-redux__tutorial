7287ec9cc5b898db3a131b167173a32dab420902
redveronika <voronovaveronika@gmail.com>
2017-08-16 23:51:23 +0300

    Add React Router to a Redux Application

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import {BrowserRouter as Router} from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 import Messages from './components/Messages';
 import TodoFrom from './components/TodoForm';
 import TodoList from "./components/TodoList";
+import Footer from "./components/Footer";
 
 class App extends Component {
   render() {
@@ -13,11 +15,14 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>React with Redux egghead course</h2>
         </div>
-        <div className="Todo-App">
-            <Messages />
-            <TodoFrom />
-            <TodoList/>
-        </div>
+          <Router>
+              <div className="Todo-App">
+                  <Messages />
+                  <TodoFrom />
+                  <TodoList/>
+                  <Footer />
+              </div>
+          </Router>
       </div>
     );
   }
