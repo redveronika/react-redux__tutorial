174a88377a33f54906b3130c2c476732d6d11275
redveronika <voronovaveronika@gmail.com>
2017-08-16 21:46:55 +0300

    Save New Records on The Server with Redux Thunk and Fetch

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,23 +1,31 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {connect} from "react-redux";
-import {updateCurrent} from '../reducers/todo';
+import {saveTodo, updateCurrent} from '../reducers/todo';
 
-const TodoForm = (props) => {
-    const {currentTodo, updateCurrent} = props;
-    const handleInputChange = (e) => {
+class TodoForm extends Component {
+    handleInputChange = (e) => {
         const val = e.target.value;
-        updateCurrent(val);
+        this.props.updateCurrent(val);
     };
-    return (
-        <form>
-            <input type="text"
-                   onChange={handleInputChange}
-                   value={currentTodo}/>
-        </form>
-    )
-};
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        this.props.saveTodo(this.props.currentTodo);
+    };
+
+    render() {
+        const {currentTodo} = this.props;
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <input type="text"
+                       onChange={this.handleInputChange}
+                       value={currentTodo}/>
+            </form>
+        )
+    }
+}
 
 export default connect(
     (state) => ({currentTodo: state.currentTodo}),
-    {updateCurrent}
+    {updateCurrent, saveTodo}
 )(TodoForm);
--- src/lib/todoServices.js
+++ src/lib/todoServices.js
@@ -2,3 +2,15 @@ export const getTodos = async () => {
     const res = await fetch('http://localhost:8080/todos');
     return res.json();
 };
+
+export const createTodo = async (name) => {
+    const res = await fetch('http://localhost:8080/todos', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({name: name, isCompleted: false})
+    });
+    return res.json();
+};
